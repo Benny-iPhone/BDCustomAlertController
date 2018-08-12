71248333132e2530fb2abdf922e9f729853b6f9f
Benny Davidovitz iOS <benny.davidovitz@gmail.com>
2018-08-12 21:06:35 +0300

    initial commit

---
+++ Example/Pods/Target Support Files/BDCustomAlertController/BDCustomAlertController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BDCustomAlertController : NSObject
+@end
+@implementation PodsDummy_BDCustomAlertController
+@end
---
+++ Example/Pods/Target Support Files/Pods-BDCustomAlertController_Example/Pods-BDCustomAlertController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BDCustomAlertController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BDCustomAlertController_Example
+@end
